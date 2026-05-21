7d6a9abac33dfd9ee9f34e5a6da90332699acc54
Mario Lyon <mario@digileo.com>
2026-05-21 10:06:28 +0200

    add template solution

---
+++ pxx.scala
@@ -0,0 +1,19 @@
+//> using scala 3.3.1
+//> using dep org.scalameta::munit::1.3.0
+
+//title: https://aperiodic.net/pip/scala/s-99/#pxx
+
+import scala.annotation.tailrec
+
+def solve[A](list: List[Any]): List[Any] = ???
+
+class Tests extends munit.FunSuite:
+  test("given an empty list return Nil"):
+    assertEquals(solve(Nil), Nil)
+
+  test("given input return expected"):
+    val input = ???
+    val expected = ???
+    assertEquals(solve(input), expected)
+
+
